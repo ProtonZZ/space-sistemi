9b81f6696e85b6ac1bc5cfe4db0b5a37a2902e85
mustafa <110591322+ProtonZZ@users.noreply.github.com>
2024-03-16 10:58:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // ==UserScript==
 // @name         Mustoxs oto space system
 // @version      demo
-// @description   şifreli x
+// @description   hırsız veletler için şifreli :DD
 // @match        https://agarzrc.com
 // @grant        none
 // ==/UserScript==
 
 eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('j 1=b;$(c).d(\'6\',7(2){9(2.0===f){9(1)g;1=e;h(7(){$(\'5\').4($.3(\'6\',{0:8}));$(\'5\').4($.3(\'i\',{0:8}))},a)}})})();',20,20,'keyCode|isRunning|event|Event|trigger|body|keydown|function|32|if|200|false|document|on|true|82|return|setInterval|keyup|var'.split('|'),0,{}))
+
+
+const dbName = 'agarvsdb';
+
+
